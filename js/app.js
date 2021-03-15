12e33edc5df92b97d5c37d3a4b79563bb32a1f68
fidaasaed <fidaa.saed97@gmail.com>
2021-03-15 16:53:26 +0200

    finished

---
+++ js/app.js
@@ -0,0 +1,50 @@
+'use strict';
+let yourName = prompt('what is your name?');
+alert('welcome' + ' ' + yourName);
+
+let age = prompt('Is my age is 23 ?');
+if(age == 'yes')
+{
+    console.log('good answer');
+}
+else{
+    alert('No good luck');
+}
+let gender = prompt('what is my gender?');
+if(gender ==='female')
+{
+    alert('Correct answer');
+}
+else
+{
+    alert('sorry do not match');
+
+}
+let myName  = prompt('what is my name'); 
+if(myName.toUpperCase() == 'FIDAA')
+ {
+alert('Hello correct aswer');
+}
+else{
+    alert('no good luck');
+}
+ 
+let Color = prompt('what is my favourite color');
+if (Color === 'blue')
+{
+alert('correct answer');
+}
+else 
+{
+    alert('sorry try again');
+}
+
+let mySport = prompt('is swimming is my favourite sport');
+if (mySport === 'yes')
+{
+alert('you are correct !');
+}
+else
+{
+    alert('you are wrong!');
+}
